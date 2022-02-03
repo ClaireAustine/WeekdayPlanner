f23c44f79878b53878a8c230f4895cbdade8b91a
Claire <cookiecak@gmail.com>
2022-02-02 19:02:11 -0800

    fixing functionality and display

--- js/script.js
+++ js/script.js
@@ -67,4 +67,12 @@ hours()
 //look up jquery ui timeblocks/hourly planner
 // can i create a for each loop that displays each hour and appends a collumn or do they all have to be separate elements in html?
 $('#9 .description').val(localStorage.getItem("9"))
+$('#10 .description').val(localStorage.getItem("10"))
+$('#11 .description').val(localStorage.getItem("11"))
+$('#12 .description').val(localStorage.getItem("12"))
+$('#13 .description').val(localStorage.getItem("13"))
+$('#14 .description').val(localStorage.getItem("14"))
+$('#15 .description').val(localStorage.getItem("15"))
+$('#16 .description').val(localStorage.getItem("16"))
+$('#17 .description').val(localStorage.getItem("17"))
 
