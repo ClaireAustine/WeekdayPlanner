78dbd9836b5e1b7aef04e5e8d02775dc77ca18e4
Claire <cookiecak@gmail.com>
2022-02-02 22:33:14 -0800

    edit readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Daily Planner
-    This is a simple daily planner I made to help better understand Jquery, JqueryUI and moment.
-    It displays the current hour in red, future hours in green and past hours in grey. 
-    Users can type in an event or task for each hour and save it so if they refresh the page it stays.
 
-    
+    This is a simple daily planner I made to help better understand Jquery, JqueryUI and moment.
+It displays the current date and standard working hours with spots to fill in events and tasks. The current hour is displayed in red, future hours in green and past hours in grey. Users can type in an event or task for each hour and save it so if they refresh the page it remains displayed.
+
+ I did this by using moment to get the current date and hour, then a for each loop to loop through each row and an if else statement to check if the hour is present, past or future. I also used jquery on() to add an event listener to the save buttons to save the description section of each row inside the local storage.
--- js/script.js
+++ js/script.js
@@ -1,50 +1,40 @@
+var currentDay = $("#currentDay");
+var timeBlocksEl = $("#timeblocks");
 
+var today = moment().format("dddd, MMMM Do");
+currentDay.text(today);
 
-var currentDay = $("#currentDay")
-var timeBlocksEl = $("#timeblocks")
+$(".saveBtn").on("click", function () {
+  var value = $(this).siblings(".description").val();
+  var key = $(this).parent().attr("id");
 
+  localStorage.setItem(key, value);
+});
 
+function hours() {
+  var currentHour = moment().hours();
 
-var today= moment().format('dddd, MMMM Do')
- currentDay.text(today);
+  $(".row").each(function () {
+    var timeBlockhour = parseInt($(this).attr("id"));
 
-$('.saveBtn').on('click', function (){
-    var value = $(this).siblings('.description').val()
-    var key = $(this).parent().attr('id')
-
-    localStorage.setItem(key , value)
-})
-
-function hours(){
-   
-    var currentHour= moment().hours();
-
-    
-
-    $('.row').each(function(){
-        var timeBlockhour = parseInt($(this).attr('id'))
-
-        if(timeBlockhour < currentHour){
-            $(this).addClass('past')
-        } else if(timeBlockhour === currentHour){
-            $(this).addClass('present')
-        } else{
-            $(this).addClass('future')
-        }
-    })
-
+    if (timeBlockhour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockhour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 }
 
-hours()
+hours();
 
-
-$('#9 .description').val(localStorage.getItem("9"))
-$('#10 .description').val(localStorage.getItem("10"))
-$('#11 .description').val(localStorage.getItem("11"))
-$('#12 .description').val(localStorage.getItem("12"))
-$('#13 .description').val(localStorage.getItem("13"))
-$('#14 .description').val(localStorage.getItem("14"))
-$('#15 .description').val(localStorage.getItem("15"))
-$('#16 .description').val(localStorage.getItem("16"))
-$('#17 .description').val(localStorage.getItem("17"))
-
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
